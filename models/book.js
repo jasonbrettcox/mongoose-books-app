2dd260450d11b46408363ba465acf4e9a02b8cd7
jasonbcox <jasonbcox@gmail.com>
2018-01-04 16:49:15 -0700

    some changes made i think

--- models/book.js
+++ models/book.js
@@ -1,17 +1,16 @@
 /// book.js
-
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+var mongoose = require ('mongoose'),
+ Schema = mongoose.Schema;
 
 var BookSchema = new Schema({
     title: String,
     author: String,
-    images: String,
-    release: String,
+    image: String,
+    releaseDate: String
 });
 
 //define the book model based on schema
-var Book = mongoose.model("Book", BookSchema);
+var Book = mongoose.model('Book', BookSchema);
 
 //exporting it so we can use it in server.js adn anywhere it is .required
 module.exports = Book;
